e7fe9320ab6dac89f250f890c24d1dcea6ddf8d1
wara <kusabashira227@gmail.com>
2015-05-16 18:21:14 +0900

    Enable specify separetor

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func usage() {
 	os.Stderr.WriteString(`
-Usage: gotran [OPTION]... [FILE]...
+Usage: ali [OPTION]... [FILE]...
 Align FILE(s), or standard input.
 
 Options:
@@ -31,17 +31,66 @@ v0.1.0
 
 var SPACES = regexp.MustCompile(`\s+`)
 
+type Separator struct {
+	re        *regexp.Regexp
+	isDefault bool
+}
+
+func NewSeparator() *Separator {
+	return &Separator{
+		isDefault: true,
+	}
+}
+
+func (s *Separator) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *Separator) Set(expr string) error {
+	re, err := regexp.Compile(regexp.QuoteMeta(expr))
+	if err != nil {
+		return err
+	}
+	s.isDefault = false
+	s.re = re
+	return nil
+}
+
+func (s *Separator) Split(t string) []string {
+	if s.isDefault {
+		return SPACES.Split(t, -1)
+	}
+
+	ils := s.re.FindAllStringIndex(t, -1)
+	if len(ils) == 0 {
+		return []string{t}
+	}
+
+	nls := make([]string, 0, len(ils)*2+1)
+	nls = append(nls, strings.TrimSpace(t[:ils[0][0]]))
+	for i := 1; i < len(ils); i++ {
+		nls = append(nls, strings.TrimSpace(t[ils[i-1][0]:ils[i-1][1]]))
+		nls = append(nls, strings.TrimSpace(t[ils[i-1][1]:ils[i][0]]))
+	}
+	nls = append(nls, strings.TrimSpace(t[ils[len(ils)-1][0]:ils[len(ils)-1][1]]))
+	nls = append(nls, strings.TrimSpace(t[ils[len(ils)-1][1]:]))
+	return nls
+}
+
 type Aligner struct {
+	sep   *Separator
 	lines [][]string
 	width []int
 }
 
 func NewAligner() *Aligner {
-	return &Aligner{}
+	return &Aligner{
+		sep: NewSeparator(),
+	}
 }
 
-func (a *Aligner) AppendLine(s string) {
-	sp := SPACES.Split(s, -1)
+func (a *Aligner) appendLine(s string) {
+	sp := a.sep.Split(s)
 	for i, cell := range sp {
 		if i == len(a.width) {
 			a.width = append(a.width, 0)
@@ -58,7 +107,7 @@ func (a *Aligner) AppendLine(s string) {
 func (a *Aligner) ReadAll(r io.Reader) error {
 	s := bufio.NewScanner(r)
 	for s.Scan() {
-		a.AppendLine(s.Text())
+		a.appendLine(s.Text())
 	}
 	return nil
 }
@@ -84,12 +133,18 @@ func do(a *Aligner, r io.Reader) error {
 	if err := a.ReadAll(r); err != nil {
 		return err
 	}
-	return a.Flush(os.Stdout)
+	a.Flush(os.Stdout)
+	return nil
 }
 
 func _main() error {
 	isHelp := flag.Bool("help", false, "")
 	isVersion := flag.Bool("version", false, "")
+
+	a := NewAligner()
+	flag.Var(a.sep, "s", "")
+	flag.Var(a.sep, "separator", "")
+
 	flag.Usage = usage
 	flag.Parse()
 	switch {
@@ -101,7 +156,6 @@ func _main() error {
 		return nil
 	}
 
-	a := NewAligner()
 	if flag.NArg() < 1 {
 		return do(a, os.Stdin)
 	}
