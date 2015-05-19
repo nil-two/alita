7fa43a4c8fa256b059ecef4f50c82f1e1173a452
wara <kusabashira227@gmail.com>
2015-05-19 23:38:53 +0900

    Enable specify just direction

--- padding.go
+++ padding.go
@@ -6,12 +6,43 @@ import (
 	"github.com/mattn/go-runewidth"
 )
 
+type Justify int
+
+const (
+	JustLeft Justify = iota
+	JustRight
+	JustCenter
+)
+
+func (j Justify) Just(width int, s string) string {
+	w := runewidth.StringWidth(s)
+	switch j {
+	case JustLeft:
+		return s + strings.Repeat(" ", width-w)
+	case JustRight:
+		return strings.Repeat(" ", width-w) + s
+	case JustCenter:
+		n := width - w
+		l, r := n/2, n/2
+		if n%2 != 0 {
+			r += 1
+		}
+		return strings.Repeat(" ", l) + s + strings.Repeat(" ", r)
+	}
+	return s + strings.Repeat(" ", width-w)
+}
+
 type Padding struct {
-	width []int
+	justfies []Justify
+	width    []int
 }
 
 func NewPadding() *Padding {
 	return &Padding{}
+}
+
+func (p *Padding) SetJustfies(a []Justify) {
+	p.justfies = a
 }
 
 func (p *Padding) UpdateWidth(a []string) {
@@ -29,13 +60,20 @@ func (p *Padding) UpdateWidth(a []string) {
 	}
 }
 
+func (p *Padding) justKind(i int) Justify {
+	if len(p.justfies) == 0 {
+		return JustLeft
+	}
+	j := (i-1)%(len(p.justfies)-1) + 1
+	return p.justfies[j]
+}
+
 func (p *Padding) Format(a []string) []string {
 	if len(a) == 1 {
 		return a
 	}
 	for i := 0; i < len(a); i++ {
-		w := p.width[i] - runewidth.StringWidth(a[i])
-		a[i] = a[i] + strings.Repeat(" ", w)
+		a[i] = p.justKind(i).Just(p.width[i], a[i])
 	}
 	return a
 }
