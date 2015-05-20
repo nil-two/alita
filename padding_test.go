8f327f68ce4ee2247cdf9b0849552347e27a3a6b
wara <kusabashira227@gmail.com>
2015-05-20 13:03:54 +0900

    Add test for justfy

--- padding_test.go
+++ padding_test.go
@@ -5,6 +5,57 @@ import (
 	"testing"
 )
 
+type JustfyTest struct {
+	justfy Justify
+	width  int
+	src    string
+	dst    string
+}
+
+var indexTestsJustfy = []JustfyTest{
+	// Case odd
+	{JustLeft, 5, "abc", "abc  "},
+	{JustRight, 5, "abc", "  abc"},
+	{JustCenter, 5, "abc", " abc "},
+
+	// Case odd && equal
+	{JustLeft, 5, "abcde", "abcde"},
+	{JustRight, 5, "abcde", "abcde"},
+	{JustCenter, 5, "abcde", "abcde"},
+
+	// Case even
+	{JustLeft, 6, "abc", "abc   "},
+	{JustRight, 6, "abc", "   abc"},
+	{JustCenter, 6, "abc", " abc  "},
+
+	// Case even && equal
+	{JustLeft, 6, "abcdef", "abcdef"},
+	{JustRight, 6, "abcdef", "abcdef"},
+	{JustCenter, 6, "abcdef", "abcdef"},
+
+	// Case min
+	{JustLeft, 2, "a", "a "},
+	{JustRight, 2, "a", " a"},
+	{JustCenter, 2, "a", "a "},
+
+	// Case over
+	{JustLeft, 4, "abcdefg", "abcdefg"},
+	{JustRight, 4, "abcdefg", "abcdefg"},
+	{JustCenter, 4, "abcdefg", "abcdefg"},
+}
+
+func TestJustfy(t *testing.T) {
+	for _, test := range indexTestsJustfy {
+		actual := test.justfy.Just(test.width, test.src)
+		expect := test.dst
+		if actual != expect {
+			kind := "lrc"[test.justfy]
+			t.Errorf("%q.Justfy(%q) = %q; want %q",
+				kind, test.src, actual, expect)
+		}
+	}
+}
+
 type UpdateWidthTest struct {
 	a      []string
 	before []int
