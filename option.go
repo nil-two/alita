4af9ed68f090e69adb1019a3cd2edfa46d6b5d9e
wara <kusabashira227@gmail.com>
2015-07-11 19:31:59 +0900

    Switch from flag to go-flag, to handle error output

--- option.go
+++ option.go
@@ -1,41 +1,27 @@
 package main
 
 import (
-	"flag"
+	"github.com/jessevdk/go-flags"
 )
 
 type Option struct {
-	Delimiter string
-	UseRegexp bool
-	Count     int
-	Margin    string
-	Justify   string
-	IsHelp    bool
-	IsVersion bool
+	Delimiter string `short:"d" long:"delimiter"`
+	UseRegexp bool   `short:"r" long:"regexp"`
+	Count     int    `short:"c" long:"count"`
+	Margin    string `short:"m" long:"margin"`
+	Justify   string `short:"j" long:"justify"`
+	IsHelp    bool   `short:"h" long:"help"`
+	IsVersion bool   `          long:"version"`
 	Files     []string
 }
 
 func ParseOption(args []string) (*Option, error) {
 	opt := &Option{}
-
-	f := flag.NewFlagSet("alita", flag.ContinueOnError)
-	f.StringVar(&opt.Delimiter, "d", "", "")
-	f.StringVar(&opt.Delimiter, "delimiter", "", "")
-	f.BoolVar(&opt.UseRegexp, "r", false, "")
-	f.BoolVar(&opt.UseRegexp, "regexp", false, "")
-	f.IntVar(&opt.Count, "c", 0, "")
-	f.IntVar(&opt.Count, "count", 0, "")
-	f.StringVar(&opt.Margin, "m", "", "")
-	f.StringVar(&opt.Margin, "margin", "", "")
-	f.StringVar(&opt.Justify, "j", "", "")
-	f.StringVar(&opt.Justify, "justify", "", "")
-	f.BoolVar(&opt.IsHelp, "h", false, "")
-	f.BoolVar(&opt.IsHelp, "help", false, "")
-	f.BoolVar(&opt.IsVersion, "version", false, "")
-
-	if err := f.Parse(args); err != nil {
+	f := flags.NewParser(opt, flags.PassDoubleDash)
+	files, err := f.ParseArgs(args)
+	if err != nil {
 		return nil, err
 	}
-	opt.Files = f.Args()
+	opt.Files = files
 	return opt, nil
 }
